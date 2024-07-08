61261f595cb49f45156e08c01c17ca8fff411a1a
cacutler <116305038+cacutler@users.noreply.github.com>
2024-07-08 11:44:43 -0600

    Added the user schema and the password methods for the user

--- model.js
+++ model.js
@@ -2,4 +2,30 @@ const mongoose = require("mongoose")
 const bcrypt = require("bcrypt")
 require("dotenv").config()
 mongoose.connect(process.env.DATABASE)
-module.exports = {}
+const userSchema = new mongoose.Schema({
+    username: {type: String, required: [true, "Username is required."]},
+    email: {type: String, required: [true, "Email is required."], unique: true},
+    encryptedPassword: {type: String, required: [true, "Password is required."]},
+    bio: String,
+    location: String,
+    displayName: String,
+    interests: Array
+})
+userSchema.methods.hashPassword = async function (password) {
+    try {
+        let hashedPassword = await bcrypt.hash(password, 12)
+        this.encryptedPassword = hashedPassword
+    } catch (error) {
+        console.log(error)
+    }
+}
+userSchema.methods.verifyPassword = async function (password) {
+    try {
+        let isGood = await bcrypt.compare(password, this.encryptedPassword)
+        return isGood
+    } catch (error) {
+        console.log(error)
+    }
+}
+const User = mongoose.model("users", userSchema)
+module.exports = {User: User}
